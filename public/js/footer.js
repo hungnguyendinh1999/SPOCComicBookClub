b3947649797dd00a81b5bfbefefcbb6da972e116
Hung Dinh Nguyen <hungnguyendinh1999@gmail.com>
2021-06-03 22:39:16 +0700

    Format Footer, fix collapse nav-bar, edit few title section, and add padding 2%

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,3 +1,3 @@
 
 
-$('#footer').append(`<footer><p>© 2021 Science-fiction People Of Clark (SPOC)</p><p>Copyright: All rights reserved</p></footer>`)
+$('#footer').append(`<footer><h1><i>Comic Book Club</i></h1><p>© 2021 Science-fiction People Of Clark (SPOC)</p><p>Copyright: All rights reserved</p></footer>`)
